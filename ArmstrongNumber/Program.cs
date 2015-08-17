4ff4a770cf2286df35fae79b32fefea2951dc0e0
Ivan Ivanov <slayer_word_11@outlook.com>
2015-08-17 20:35:26 +0300

    init algo project and add 2 algo

---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ArmstrongNumber
+{
+    using System;
+
+    class ArmstrongNumber
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a one number: ");
+            int num = int.Parse(Console.ReadLine());
+            int n = num;
+            int check = 0;
+            int remainder;
+            while (num > 0)
+            {
+                remainder = num % 10;
+                check = check + (int) Math.Pow(remainder, 3);
+                num = num / 10;
+            }
+            if (check == n)
+            {
+                Console.WriteLine(n + " is an Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine(n + " is not a Armstrong Number");
+            }
+        }
+    }
+
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace BubbleSort
+{
+    using System;
+    class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+
+            int[] intArray = new int[] { 5, 90, 35, 45, 150, 3 };
+
+            Console.WriteLine("Besore bubble sort");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            bubbleSort(intArray);
+
+            Console.WriteLine("After bubble sort");
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+        }
+        private static void bubbleSort(int[] intArray)
+        {
+            int n = intArray.Length;
+            int temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j < (n - i); j++)
+                {
+                    if (intArray[j - 1] > intArray[j])
+                    {
+                        temp = intArray[j - 1];
+                        intArray[j - 1] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
