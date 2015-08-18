8c978b407cb2faab52c5bb6ce912d0d77086d498
Ivan Ivanov <slayer_word_11@outlook.com>
2015-08-18 10:52:05 +0300

    add 2 algo

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,20 @@
+﻿namespace Factorial
+{
+    using System;
+
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a one number: ");
+            int num = int.Parse(Console.ReadLine());
+            int result = 1;
+            while (num > 0)
+            {
+                result = result * num;
+                num--;
+            }
+           Console.WriteLine("Factorial is: " + result);
+        }
+    }
+}
---
+++ FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    class FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a one number: ");
+            int num = int.Parse(Console.ReadLine());
+            int f1;
+            int f2 = 0;
+            int f3 = 1;
+
+            Console.WriteLine("Fibonacci numbers");
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(f3);
+                f1 = f2;
+                f2 = f3;
+                f3 = f1 + f2;
+            }
+        }
+    }
+}
